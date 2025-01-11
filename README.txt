c7734aed8926a9ebcaad3da51e1b3201e636deb1
AlandisAyupov <steamuservni@gmail.com>
2025-01-11 17:44:37 -0500

    requirements.txt + streamlit + README

---
+++ README.txt
@@ -0,0 +1,16 @@
+Instructions for running the application.:
+
+1. Create a python virtual environment + activate virtual environment.
+    -See the link on how to do both: https://python.land/virtual-environments/virtualenv 
+
+2. Run 'pip install -r requirements.txt' in the terminal. 
+    -This will install a list of required project dependencies. 
+
+3. Running the project files:
+
+    Streamlit:
+        -In order to run streamlit.py, run 'streamlit run streamlit.py' in the terminal.
+
+    Government Debt and Spending:
+        -There are several ways you can run this file, check the link below:
+        https://www.geeksforgeeks.org/how-to-run-an-ipynb-jupyter-notebook-from-terminal/
--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,20 @@
+# IMPORTS
 import pandas as pd
 import streamlit as st
+
+# LOADING DATA
+econ_df = pd.read_csv('data/CombinedData.csv')
+
+# DATA CREATION
+econ_df['net_income'] = econ_df['receipts'] - econ_df['expenditure']
+econ_df['deficit_spending'] = (econ_df['receipts'] - econ_df['expenditure']) * -1
+econ_df['interest_percent_of_gdp'] = econ_df['interest_expenditure']/econ_df['gdp'] * 100
+econ_df['debt_percent_of_gdp'] = (econ_df['govt_debt']/1000)/econ_df['gdp'] * 100
+econ_df['interest_percent_of_expenditure'] = econ_df['interest_expenditure']/econ_df['expenditure'] * 100
+econ_df['projected_interest_payments'] = ((econ_df['fed_funds_rate']/100) * (econ_df['govt_debt']/1000))/econ_df['gdp'] * 100
+
+st.write("""
+# Deficit Spending
+""")
+ 
+st.line_chart(econ_df['deficit_spending'])
