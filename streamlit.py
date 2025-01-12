0e13e80ca87a186589b0b151b88c971a97a4fa6d
AlandisAyupov <steamuservni@gmail.com>
2025-01-12 00:41:03 -0500

    Streamlit multipage charts.

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 # IMPORTS
 import pandas as pd
+import altair as alt
 import streamlit as st
 
 # LOADING DATA
@@ -13,8 +14,65 @@ econ_df['debt_percent_of_gdp'] = (econ_df['govt_debt']/1000)/econ_df['gdp'] * 10
 econ_df['interest_percent_of_expenditure'] = econ_df['interest_expenditure']/econ_df['expenditure'] * 100
 econ_df['projected_interest_payments'] = ((econ_df['fed_funds_rate']/100) * (econ_df['govt_debt']/1000))/econ_df['gdp'] * 100
 
-st.write("""
-# Deficit Spending
-""")
- 
-st.line_chart(econ_df['deficit_spending'])
+# CHARTS
+def gdp():
+    st.write("""
+    # GDP
+    """)
+    chart = alt.Chart(econ_df).mark_line().encode(
+        x=alt.X('observation_date:T', title='Date'),
+        y=alt.Y('gdp:Q', title='GDP (Billions of Dollars)')
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+def deficit_spending_chart():
+    st.write("""
+    # Deficit Spending
+    """)
+    chart = alt.Chart(econ_df).mark_line().encode(
+        x=alt.X('observation_date:T', title='Date'),
+        y=alt.Y('deficit_spending:Q', title='Deficit Spending (Billions of Dollars)')
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+def interest_percent_of_gdp_chart():
+    st.write("""
+    # Interest Payments as Percent of GDP
+    """)
+    chart = alt.Chart(econ_df).mark_line().encode(
+        x=alt.X('observation_date:T', title='Date'),
+        y=alt.Y('interest_percent_of_gdp:Q', title='Interest Percent of GDP')
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+def debt_percent_of_gdp_chart():
+    st.write("""
+    # Debt as Percent of GDP
+    """)
+    chart = alt.Chart(econ_df).mark_line().encode(
+        x=alt.X('observation_date:T', title='Date'),
+        y=alt.Y('debt_percent_of_gdp:Q', title='Debt Percent of GDP')
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+def interest_percent_of_expenditure_chart():
+    st.write("""
+    # Interest Payments as Percent of Expenditure
+    """)
+    chart = alt.Chart(econ_df).mark_line().encode(
+        x=alt.X('observation_date:T', title='Date'),
+        y=alt.Y('interest_percent_of_expenditure:Q', title='Interest Percent of Expenditure')
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+# SIDEBAR
+pages = {
+    "Gross Domestic Product": gdp,
+    "Deficit Spending": deficit_spending_chart,
+    "Interest Percent of GDP": interest_percent_of_gdp_chart,
+    "Debt Percent of GDP": debt_percent_of_gdp_chart,
+    "Interest Percent of Expenditure": interest_percent_of_expenditure_chart
+}
+
+demo = st.sidebar.selectbox("Chart", options=pages.keys(), format_func=lambda x: str(x))
+pages[demo]()
